f8bd181ab40f3d03cfde456ca52d8de676435f55
Johann Dee <johanndee1994@gmail.com>
2023-11-09 14:21:54 -0800

    Update pet card image size

--- src/components/PetFinder/PetFinder.js
+++ src/components/PetFinder/PetFinder.js
@@ -52,12 +52,12 @@ function PetFinder() {
           {results.map((pet) => (
             <div className="pet-card md:w-1/2 lg:w-1/4 p-4" key={pet.id}>
               {pet.photos && pet.photos[0] && pet.photos[0].full && (
-                <img className="pet-card-img w-full h-48 object-cover" 
+                <img className="pet-card-img w-full h-72 object-cover" 
                 src={pet.photos[0].full} 
                 alt={pet.name} 
                 />
                 )}
-                <div className="pet-card-info flex flex-col  items-center text-sm w-full">
+                <div className="pet-card-info flex flex-col items-center text-sm w-full">
                   <h3>{pet.name} · {pet.gender[0]}</h3>
                   <div className="text-xs">{pet.age} · {pet.breeds.primary}</div>
                 </div>
