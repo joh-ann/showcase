d9fc703dbb6458c736b0933c99e7550d6e8d5094
Johann Dee <johanndee1994@gmail.com>
2023-11-12 02:38:56 -0800

    Add petinfo fixture

---
+++ cypress/e2e/petinfo.cy.js
@@ -0,0 +1,73 @@
+describe('PetInfo', () => {
+  beforeEach(() => {
+    cy.intercept('POST', 'https://api.petfinder.com/v2/oauth2/token', {
+      statusCode: 200,
+      body: {
+        access_token: 'sampleToken',
+        token_type: 'Bearer',
+        expires_in: 3600,
+      },
+    }).as('getToken');
+
+    cy.intercept('https://maps.googleapis.com/maps/api/js*', (req) => {
+      req.reply({
+        statusCode: 200,
+        body: 'console.log("Google Maps API script loaded successfully");',
+      });
+    }).as('googleMapsAPI');
+
+    cy.intercept('GET', 'https://api.petfinder.com/v2/animals*', 
+      {
+        statusCode: 200,
+        fixture: 'pets.json' 
+      }
+    ).as('getPetResults');
+
+    cy.intercept('GET', 'https://api.petfinder.com/v2/animals/69650492',
+      {
+        statusCode: 200,
+        fixture: 'petinfo.json'
+      }).as('getPetInfo');
+    
+    cy.visit('http://localhost:3000/pets/1');
+    
+    cy.wait('@getToken');
+    cy.wait('@googleMapsAPI');
+    cy.wait('@getPetResults');
+  });
+
+  it('should be able to display info of a clicked pet', () => {
+    cy.url().should('include', '/pets/1');
+
+    cy.get('.header').should('exist');
+
+    cy.get('.header').contains('Home');
+    cy.get('.header').contains('Adopt');
+    cy.get('.header').contains('Missing');
+    cy.get('.header').contains('Events');
+
+    cy.get('.pet-card').should('have.length', 3);
+
+    cy.get('.pet-card').eq(0).should('have.attr', 'id', '69650492');
+    cy.get('.pet-card-img').eq(0).should('exist');
+    cy.get('.pet-card').eq(0).contains('Ella · F');
+    cy.get('.pet-card').eq(0).contains('The Woodlands, TX');
+    cy.get('.pet-card').eq(0).contains('Baby · Domestic Medium Hair');
+
+    cy.get('.pet-card').eq(1).should('have.attr', 'id', '69650489');
+    cy.get('.pet-card-img').eq(1).should('exist');
+    cy.get('.pet-card').eq(1).contains('EMBER · F');
+    cy.get('.pet-card').eq(1).contains('The Woodlands, TX');
+    cy.get('.pet-card').eq(1).contains('Baby · Domestic Short Hair');
+
+    cy.get('.pet-card').eq(2).should('have.attr', 'id', '69600670');
+    cy.get('.pet-card-img').eq(2).should('exist');
+    cy.get('.pet-card').eq(2).contains('Twix Cocoapup · M');
+    cy.get('.pet-card').eq(2).contains('Woodinville, WA');
+    cy.get('.pet-card').eq(2).contains('Baby · Australian Cattle Dog');
+    
+    cy.get('.pet-card').eq(0).should('have.attr', 'id', '69650492').click();
+    cy.wait('@getPetInfo');
+    cy.url().should('eq', 'http://localhost:3000/pets/1/69650492');
+  });
+});
