c2de36d48519805462c75332819baebec73cbda7
Johann Dee <johanndee1994@gmail.com>
2023-11-12 20:55:21 -0800

    Update petinfo styling

--- src/components/PetInfo/PetInfo.js
+++ src/components/PetInfo/PetInfo.js
@@ -44,7 +44,7 @@ function PetInfo() {
   };
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col h-full bg-green-50">
       {petInfo.photos && petInfo.photos.length > 0 && petInfo.photos[0].full && (
         <img 
           className="pet-info-img w-1/4 flex self-center mt-5 rounded-t-2xl"
@@ -59,29 +59,29 @@ function PetInfo() {
             alt={petInfo.name} 
           />
           )}
-          <div className="pet-info-wrapper bg-green-200 flex flex-col self-center w-1/4 rounded-b-2xl p-1 pb-4">
+          <div className="pet-info-wrapper bg-green-300 flex flex-col self-center w-1/4 rounded-b-2xl p-1 pb-4 shadow-md">
             <div className="pet-info self-center text-lg font-semibold text-center">{petInfo.name}</div>
             <div className="pet-info self-center text-sm text-center">{petInfo.age} · {petInfo.gender} · {petInfo.size}</div>
             <div className="pet-info self-center text-sm text-center">{petInfo.breeds.primary} · {petInfo.contact.address.city}, {petInfo.contact.address.state}</div>
           </div>
-        <div className="container flex self-center flex-row">
-          <div className="pet-info w-1/2 p-2">
-            <h2 className="text-xl font-semibold">About</h2>
+        <div className="container flex self-center flex-row m-10 bg-gray-600 text-white rounded-xl shadow-md">
+          <div className="pet-info w-1/2 p-8">
+            <h2 className="text-2xl font-semibold mb-4">About</h2>
               <div className="flex flex-col">
-                <p>Characteristics</p>
-                <p className="flex flex-row text-sm">{petInfo.tags && petInfo.tags.join(", ")}</p>
-                <p>Coat Length</p>
-                <p className="flex flex-row text-sm">{petInfo.coat}</p>
-                <p>House-Trained</p>
-                <p className="flex flex-row text-sm">{petInfo.attributes.house_trained ? "Yes" : "No"}</p>
-                <p>Health</p>
+                <p className="text-sm font-semibold">CHARACTERISTICS</p>
+                <p className="flex flex-row text-sm mb-3">{petInfo.tags && petInfo.tags.join(", ")}</p>
+                <p className="text-sm font-semibold">COAT LENGTH</p>
+                <p className="flex flex-row text-sm mb-3">{petInfo.coat}</p>
+                <p className="text-sm font-semibold">HOUSE-TRAINED</p>
+                <p className="flex flex-row text-sm mb-3">{petInfo.attributes.house_trained ? "Yes" : "No"}</p>
+                <p className="text-sm font-semibold">HEALTH</p>
                 <p className="flex flex-row text-sm">{petInfo.attributes.shots_current ? "Vaccinations up to date" : "Needs Vaccinations"}, {petInfo.attributes.spayed_neutered ? "spayed/neutered" : "Not spayed/neutered"}</p>
               </div>
             </div>
-            <div className="pet-intro w-1/2 p-2">
-              <h2 className="text-xl font-semibold">Meet {petInfo.name}</h2>
-              <p>{petInfo.description}</p>
-              <div className="flex justify-end p-2">
+            <div className="pet-intro w-1/2 p-8">
+              <h2 className="text-2xl font-semibold mb-4">Meet {petInfo.name}</h2>
+              <p className="text-sm">{petInfo.description}</p>
+              <div className="flex justify-end mt-20">
                 <button 
                   className="petfinder-btn bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow"
                   onClick={handleViewOnPetFinder}
